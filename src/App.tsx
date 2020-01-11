82da34242a719430302e412f5e7a080a8e9f3a90
sitogi <takano162@gmail.com>
2020-01-12 00:31:57 +0900

    update to timer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,65 @@
 import React, { Component } from "react";
-import { Button, Card, Statistic } from "semantic-ui-react";
+import { Button, Card, Icon, Statistic } from "semantic-ui-react";
 
 import "./App.css";
 
+const LIMIT = 60;
+
 // Local State を定義しておく
 interface AppState {
-  count: number;
+  timeLeft: number;
 }
 
 class App extends Component<{}, AppState> {
   // 1 つ目が Props で、 2 つ目が Local State
   constructor(props: {}) {
     super(props);
-    this.state = { count: 0 }; // Local State の初期化が必要
+    this.state = { timeLeft: LIMIT }; // Local State の初期化が必要
   }
 
   // State の更新は必ず this.setState を使う
-  increment = () => this.setState(prevState => ({ count: prevState.count + 1 }));
+  tick = () => this.setState(prevState => ({ timeLeft: prevState.timeLeft - 1 }));
 
   // setState の引数は、 以下のいずれか
   // 1. 設定したい値の State オブジェクト
   // 2. (prevState, props) => newState 形式の、State と Props を受け取って新しい State を返す関数
-  decrement = () => this.setState(prevState => ({ count: prevState.count - 1 }));
+  reset = () => this.setState(prevState => ({ timeLeft: LIMIT }));
+
+  timerId?: NodeJS.Timer;
+
+  componentDidMount = () => {
+    this.timerId = setInterval(this.tick, 1000);
+  };
+
+  componentDidUpdate = () => {
+    const { timeLeft } = this.state;
+    if (timeLeft === 0) {
+      this.reset();
+    }
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerId as NodeJS.Timer);
+  };
 
   render() {
-    const { count } = this.state;
+    const { timeLeft } = this.state;
 
     return (
       <div className="container">
         <header>
-          <h1>Counter</h1>
+          <h1>Timer</h1>
         </header>
         <Card>
           <Statistic className="number-board" color="teal">
-            <Statistic.Label>count</Statistic.Label>
-            <Statistic.Value>{count}</Statistic.Value>
+            <Statistic.Label>TIME</Statistic.Label>
+            <Statistic.Value>{timeLeft}</Statistic.Value>
           </Statistic>
           <Card.Content>
-            <div className="ui two buttons">
-              <Button color="red" onClick={this.decrement}>
-                -1
-              </Button>
-              <Button color="green" onClick={this.increment}>
-                +1
-              </Button>
-            </div>
+            <Button color="red" fluid onClick={this.reset}>
+              <Icon name="redo"></Icon>
+              Reset
+            </Button>
           </Card.Content>
         </Card>
       </div>
