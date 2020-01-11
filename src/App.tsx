a72e9a14d0bd95ba1db55d1f1e2cf8ff04b4090a
sitogi <takano162@gmail.com>
2020-01-11 23:54:00 +0900

    update to counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,57 @@
 import React, { Component } from "react";
-import CharacterList, { Character } from "./CharacterList";
+import { Button, Card, Statistic } from "semantic-ui-react";
 
 import "./App.css";
 
-class App extends Component {
+interface AppState {
+  count: number;
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment() {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+
+  decrement() {
+    this.setState(prevState => ({
+      count: prevState.count - 1
+    }));
+  }
+
   render() {
-    const characters: Character[] = [
-      {
-        id: 1,
-        name: " 羽咲 綾乃 ",
-        age: 16,
-        height: 151
-      },
-      {
-        id: 2,
-        name: " 荒垣 なぎさ ",
-        age: 18,
-        height: 174
-      },
-      {
-        id: 3,
-        name: " 泉 理子 ",
-        age: 18
-      }
-    ];
+    const { count } = this.state;
+
     return (
       <div className="container">
         <header>
-          <h1>はねバド!キャラクター一覧</h1>
+          <h1>Counter</h1>
         </header>
-        <CharacterList school="北小町高校" characters={characters} />
+        <Card>
+          <Statistic className="number-board" color="teal">
+            <Statistic.Label>count</Statistic.Label>
+            <Statistic.Value>{count}</Statistic.Value>
+          </Statistic>
+          <Card.Content>
+            <div className="ui two buttons">
+              <Button color="red" onClick={() => this.decrement()}>
+                -1
+              </Button>
+              <Button color="green" onClick={() => this.increment()}>
+                +1
+              </Button>
+            </div>
+          </Card.Content>
+        </Card>
       </div>
     );
   }
 }
+
 export default App;
