27c4f10e9c401e3115e3234b7b9c13e24b25028e
sitogi <takano162@gmail.com>
2020-01-12 14:51:45 +0900

    change Class Component to Function Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
+import React, { FC } from "react";
 import Hanebado from "./Hanebado";
 import ClickCounter from "./ClickCounter";
 import CountDownTimer from "./CountDownTimer";
 
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="ui container three column grid segment">
-        <Hanebado></Hanebado>
-        <ClickCounter></ClickCounter>
-        <CountDownTimer></CountDownTimer>
-      </div>
-    );
-  }
-}
+const App: FC = () => (
+  <div className="ui container three column grid segment">
+    <Hanebado></Hanebado>
+    <ClickCounter></ClickCounter>
+    <CountDownTimer></CountDownTimer>
+  </div>
+);
 
 export default App;
--- src/CountDownTimer.tsx
+++ src/CountDownTimer.tsx
@@ -1,70 +1,43 @@
-import React, { Component } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { Button, Card, Icon, Statistic } from "semantic-ui-react";
 
 import "./App.css";
 
 const LIMIT = 60;
 
-// Local State を定義しておく
-interface AppState {
-  timeLeft: number;
-}
+const CountDownTimer: FC = () => {
+  const [timeLeft, setTimeLeft] = useState(LIMIT);
 
-class CountDownTimer extends Component<{}, AppState> {
-  // 1 つ目が Props で、 2 つ目が Local State
-  constructor(props: {}) {
-    super(props);
-    this.state = { timeLeft: LIMIT }; // Local State の初期化が必要
-  }
+  const tick = () => setTimeLeft(prevTime => (prevTime === 0 ? LIMIT : prevTime - 1));
+  const reset = () => setTimeLeft(LIMIT);
 
-  // State の更新は必ず this.setState を使う
-  tick = () => this.setState(prevState => ({ timeLeft: prevState.timeLeft - 1 }));
+  // Efect Hook。第一引数は引数なしの関数、第二引数は配列を渡す。
+  useEffect(() => {
+    const timerId = setInterval(tick, 1000); // componentDidMount()
 
-  // setState の引数は、 以下のいずれか
-  // 1. 設定したい値の State オブジェクト
-  // 2. (prevState, props) => newState 形式の、State と Props を受け取って新しい State を返す関数
-  reset = () => this.setState(prevState => ({ timeLeft: LIMIT }));
+    return () => clearInterval(timerId); // componentWillUnmount()
+  }, []); // 第二引数で渡した配列の値が前回と変更されていれば、 setInterval が呼ばれる。
+  // 今回はからの配列を渡しているので、 setInterval は初回しか呼ばれない。
 
-  timerId?: NodeJS.Timer;
-
-  componentDidMount = () => {
-    this.timerId = setInterval(this.tick, 1000);
-  };
-
-  componentDidUpdate = () => {
-    const { timeLeft } = this.state;
-    if (timeLeft === 0) {
-      this.reset();
-    }
-  };
-
-  componentWillUnmount = () => {
-    clearInterval(this.timerId as NodeJS.Timer);
-  };
-
-  render() {
-    const { timeLeft } = this.state;
-
-    return (
-      <div className="container">
-        <header>
-          <h1>Timer</h1>
-        </header>
-        <Card>
-          <Statistic className="number-board" color="teal">
-            <Statistic.Label>TIME</Statistic.Label>
-            <Statistic.Value>{timeLeft}</Statistic.Value>
-          </Statistic>
-          <Card.Content>
-            <Button color="red" fluid onClick={this.reset}>
-              <Icon name="redo"></Icon>
-              Reset
-            </Button>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <header>
+        <h1>Timer</h1>
+      </header>
+      <Card>
+        <Statistic className="number-board" color="teal">
+          <Statistic.Label>TIME</Statistic.Label>
+          <Statistic.Value>{timeLeft}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <Button color="red" fluid onClick={reset}>
+            <Icon name="redo"></Icon>
+            Reset
+          </Button>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
 
 export default CountDownTimer;
